4d717ced946317d6bec5ce39a7cc69c289c55419
Justin <justinc4@gmail.com>
2013-01-21 18:19:10 -0500

    Initial thoughts and class naming.  DINNER time is more important.

--- dub-main.js
+++ dub-main.js
@@ -1,25 +1,85 @@
-DubMark = Ember.Application.create({
+DubMark = Em.Application.create({
   ready: function(){
     console.log("Application is actually ready to start.");
   }
 });
 
-DubMark.SubModel = Ember.Object.extend({
+DubMark.SubModel = Em.Object.extend({
   start: '',
   end: '',
   text: ''
 });
-DubMark.ApplicationView = Ember.View.extend({
+DubMark.ApplicationView = Em.View.extend({
   templateName: 'application'
 });
-DubMark.ApplicationController = Ember.ArrayController.extend({
+DubMark.ApplicationController = Em.ArrayController.extend({
   content: []
 });
-DubMark.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-    index: Ember.Route.extend({
+DubMark.Router = Em.Router.extend({
+  root: Em.Route.extend({
+    index: Em.Route.extend({
       route: '/'
     })
   })
 });
 DubMark.app = DubMark.ApplicationController.create();
+
+
+/**
+ *
+ */
+DubMark.Project = function(init){
+  this._init = init;
+};
+$.extend(DubMark.Project.prototype, {
+  connect: function(init){
+    var vid = $('#' + this._init.id);
+    if(vid){
+      var vc = this.getVideoCtrl();
+          vc.listen(vid);
+    }else{
+      console.error("Failed to find a video with this init data.", this._init);
+    }
+  },
+  createNew: function(init){
+    //Create a fully new page and probably kick off a reload
+  },
+  loadId: function(id){
+    //Ajax call, get project info and load
+  },
+  getSubCtrl: function(){
+
+  },
+  getVideoCtrl: function(){
+    if(!this._vidCtrl){
+      this._vidCtrl = new DubMark.Video();
+    }
+    return this._vidCtrl;
+  }
+});
+
+
+/**
+ * For the control of various video timing issues.
+ */
+DubMark.Video = function(){
+
+};
+$.extend(DubMark.Video.prototype,{
+  listen: function(vid){
+    this._vid = vid;
+    //Connect the event based pipes.
+  },
+  play: function(){
+
+  },
+  pause: function(){ //Pause the video
+    
+  },
+  getTime: function(){ //Time the video is currently at
+
+  },
+  getLength: function(){ //Length of the entire vid
+
+  }
+});
