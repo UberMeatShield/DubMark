42bedbddbcc54d7925845a66ec558e4f65ec0d56
Justin Carlson <justinc4@gmail.com>
2013-05-21 17:34:38 -0400

    Use the directive code to represent state.

--- app/assets/javascripts/dub-widgets.js
+++ app/assets/javascripts/dub-widgets.js
@@ -1,26 +1,30 @@
-
-DubMark.Modules.Dub.directive("stats", function() {
-    return {
-      restrict: "E",
-      transclude: true,
-      scope: {},
-      controller: function($scope, $element) {
-        console.log("Stats directive scope and element.", $scope, $element);
-      },
-      template: 
-        '<div class="span1" ng-class="list.isComplete(key)">' +
-          '<img ng-class="list.getStateIcon(key)" title="{{key}} {{list.active.status[key]}}"  />' +
-        '</div>',
-/*
-        '<div class="tabbable">' +
-          '<ul class="nav nav-tabs">' +
-            '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
-            '</li>' +
-          '</ul>' +
-          '<div class="tab-content" ng-transclude></div>' +
-        '</div>',
-*/
-      replace: true
-    };
+DubMark.Modules.Dub.directive("status", function() {
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: true,
+    controller: function($scope, $element) {
+      console.log("Scope args", $scope);
+      //How do I get the scope of the item hacked in?
+      $scope.isComplete = function(key){
+        if($scope.proj.status[key]){
+          return 'completed';
+        }
+      };
+      $scope.getStateIcon = function(key){
+        return DubMark.States[key];
+      };
+    },
+    template: 
+    '<div class="container-fluid">' +
+      '<div class="row-fluid">' +
+        '<div class="text-center " ng-repeat=\'key in ["New", "Timed", "Translated", "QA", "Completed",           "Published"]\' >' +
+          '<div class="span2" ng-class="isComplete(key)">' +
+          '<img ng-class="getStateIcon(key)" />' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+    '</div>',
+    replace: true
+  };
 });
